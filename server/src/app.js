8dbc33ca99d415c4d680b7c50b1acd1244fb1bc5
ahsanraza161 <ahsanrazainfo.com>
2023-12-23 13:54:57 +0500

    last time trying to connect backend with vercel

--- server/src/app.js
+++ server/src/app.js
@@ -11,6 +11,12 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 app.use(cors({ origin: '*' }));
+app.use(cors({ "headers": [
+  { "key": "Access-Control-Allow-Credentials", "value": "true" },
+  { "key": "Access-Control-Allow-Origin", "value": "*" },
+  { "key": "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  { "key": "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+] }));
 app.use(bodyParser.json());
 app.use(AuthRouters);
 const httpServer = createServer(app);
